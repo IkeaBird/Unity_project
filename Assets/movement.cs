d1826baf65bea86ad0197ce97cfbc1d67de91012
21gfrhug <21gfrhug@soltorgsgymnasiet.se>
2023-05-03 14:55:20 +0200

    Slight change

--- Assets/movement.cs
+++ Assets/movement.cs
@@ -18,21 +18,27 @@ public class movement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if ()
+        {
+
+        }
+
+
         if (Input.GetKeyDown(moveUp))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 10);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 5);
         }
         if (Input.GetKeyDown(moveDown))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -10);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -5);
         }
         if (Input.GetKeyDown(moveLeft))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-5, 0);
         }
         if (Input.GetKeyDown(MoveRight))
         {
-            GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
+            GetComponent<Rigidbody2D>().velocity = new Vector2(5, 0);
         }
     }
 }
