84cae7729c12b00ff1ee033990d01d8ba25361ab
21gfrhug <21gfrhug@soltorgsgymnasiet.se>
2023-05-03 13:54:44 +0200

    Bing chilling

---
+++ Assets/movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour
+{
+    public KeyCode moveDown;
+    public KeyCode moveUp;
+    public KeyCode moveLeft;
+    public KeyCode MoveRight;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(moveUp))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, 10);
+        }
+        if (Input.GetKeyDown(moveDown))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(0, -10);
+        }
+        if (Input.GetKeyDown(moveLeft))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(-10, 0);
+        }
+        if (Input.GetKeyDown(MoveRight))
+        {
+            GetComponent<Rigidbody2D>().velocity = new Vector2(10, 0);
+        }
+    }
+}
